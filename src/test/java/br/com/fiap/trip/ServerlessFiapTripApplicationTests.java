a69738f5e16fb8aa98ae32e55e3ad3fd81c2a19a
Bruno Aguiar <bruno12_94@hotmail.com>
2020-05-06 01:17:06 -0300

    sam and tests

--- src/test/java/br/com/fiap/trip/ServerlessFiapTripApplicationTests.java
+++ src/test/java/br/com/fiap/trip/ServerlessFiapTripApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.fiap.trip;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerlessFiapTripApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
